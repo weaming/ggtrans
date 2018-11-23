43ff4761fbee808b72da8f0cde361f5df1ba3ea0
weaming <garden.yuen@gmail.com>
2018-11-23 20:01:13 +0800

    simplify output; accept read file as text

--- translate.js
+++ translate.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 
 const translate = require('google-translate-api');
+const fs = require('fs');
+
 
 let cli_args = process.argv.slice(2);
 let p = console.log.bind(console);
@@ -8,10 +10,13 @@ let p = console.log.bind(console);
 
 if (cli_args.length >= 1) {
   let text = cli_args[0];
+  if (text[0] == "/") {
+    text = fs.readFileSync(text, 'utf8')
+  }
   let to;
 
   if (cli_args.length == 1) {
-    if (text.match(/[a-z ,.;'"!?(){}\[\]\n\r\t]/i)) {
+    if (text.match(/[a-z ,.;/\\'"!?(){}\[\]\n\r\t]/i)) {
       to = 'zh-cn';
     } else {
       to = 'en';
@@ -21,14 +26,15 @@ if (cli_args.length >= 1) {
   }
 
   translate(text, {to: to}).then(res => {
-    p(`From : ${res.from.language.iso}`)
-    p(`  To : ${to}`)
-    if (res.from.text.autoCorrected) {
-      p(`Text : ${res.from.text.value}`)
-    }
-    p(`---------------`)
+    // p(`From : ${res.from.language.iso}`)
+    // p(`  To : ${to}`)
+    // if (res.from.text.autoCorrected) {
+    //   p(`Text : ${res.from.text.value}`)
+    // }
+    // p(`---------------`)
+    p(text.trim())
     p(res.text);
-    p()
+    // p()
   }).catch(err => {
     console.error(err);
   });
